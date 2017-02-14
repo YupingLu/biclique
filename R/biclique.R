81984517e438533bd72fb5620346cf4a2f60448d
YupingLu <yupinglu@outlook.com>
2017-02-14 14:33:53 -0500

    update biclique.R
    change output format

--- R/biclique.R
+++ R/biclique.R
@@ -29,10 +29,10 @@ biclique <- function(filename, lleast = 1, rleast = 1, version = 1, getclique = 
     nelems = profile.raw[1]
 
     # paste profile data into profile
-    profile = paste("|Right Vertex|\t|Left Vertex|\tNumber\n")
+    profile = paste(" biclique \t Number \n")
 
    	for (i in seq(1, nelems-9, 3)){
-   	    profile = paste(profile, profile.raw[i+1], "\t", profile.raw[i+2], "\t", profile.raw[i+3], "\n", sep="")
+   	    profile = paste(profile, "K", profile.raw[i+1], ",", profile.raw[i+2], "\t", profile.raw[i+3], "\n", sep="")
    	}
 
    	profile = paste(profile, "\n", sep="")
@@ -40,8 +40,8 @@ biclique <- function(filename, lleast = 1, rleast = 1, version = 1, getclique = 
     profile = paste(profile, "Number of right vertices    : ", profile.raw[nelems-6], "\n", sep="")
     profile = paste(profile, "Number of edges             : ", profile.raw[nelems-5], "\n", sep="")
     profile = paste(profile, "Number of bicliques         : ", profile.raw[nelems-4], "\n", sep="")
-    profile = paste(profile, "Size of edge max biclique   : ", "(", profile.raw[nelems-3], ",", profile.raw[nelems-2], ")", "\n", sep="")
-    profile = paste(profile, "Size of vertex max biclique : ", "(", profile.raw[nelems-1], ",", profile.raw[nelems], ")", "\n", sep="")
+    profile = paste(profile, "Maximum edge biclique       : ", "K", profile.raw[nelems-3], ",", profile.raw[nelems-2], "\n", sep="")
+    profile = paste(profile, "Maximum vertex biclique     : ", "K", profile.raw[nelems-1], ",", profile.raw[nelems], "\n", sep="")
 
     # print profile
     on.exit(cat(profile))
