c832185ae4a3ba5cfae53d219a73e9fa27d0d2f8
YupingLu <yupinglu@outlook.com>
2017-05-04 02:11:39 -0400

    update package title

--- R/package-biclique.R
+++ R/package-biclique.R
@@ -5,7 +5,7 @@
 #' @useDynLib biclique, .registration = TRUE
 #'
 #' @docType package
-#' @title Maximal Complete Bipartite Graphs
+#' @title Maximal Biclique Enumeration in Bipartite Graphs
 #'
 #' @description A tool for enumerating maximal complete bipartite graphs. The input should be a edge list file or a binary matrix file. The output are maximal complete bipartite graphs. Algorithms used can be found in this paper Y Zhang et al. BMC Bioinformatics 2014 15:110 <doi: 10.1186/1471-2105-15-110>.
 #'
